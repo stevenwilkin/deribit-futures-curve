e762c607e31bffdf7aafcc7357e5ae9b9c50d52d
Steven Wilkin <stevenwilkin@gmail.com>
2021-06-24 19:45:15 +0700

    Render yields as a line chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import "../css/app.css"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -13,3 +13,36 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+
+import Chart from 'chart.js/auto';
+import 'chartjs-adapter-date-fns';
+import format from 'date-fns/format'
+
+var ctx = document.getElementById('chart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: {
+    scales: {
+      x: {
+        type: 'time',
+      },
+    },
+    plugins: {
+      legend: {
+        display: false
+      },
+      tooltip: {
+        displayColors: false,
+        callbacks: {
+          title: function(context) {
+            return context[0].parsed.y + '%';
+          },
+          label: function(context) {
+            return format(context.raw.x, 'do LLLL Y');
+          },
+        }
+      }
+    }
+  }
+});
